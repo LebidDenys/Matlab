b8d585b1ca0e834a9fb09da02c52992de97ef957
Denis Lebed <Denis.Lebid7@gmail.com>
2016-12-20 23:22:59 +0200

    added new tasks

--- modul 1/task2.m
+++ modul 1/task2.m
@@ -1,26 +1,27 @@
-y_values = [-1,-1.8,-2.5,0,3,2.5,1]; %initial data
+y_values = [-1,-1.8,-2.5,0,3,2.5,1];%initial data
+%1,1.8,2.6,0.5,-3,-2.5,1
 x_values = (0: pi/300: 2*pi);
 x_actual = (0: pi/3: 2*pi);
 
-y_matrix = zeros(2, 1); %creating empty matrix
+y_matrix = zeros(2, 1);%creating empty matrix
 x_matrix = zeros(2, 2);
 
-for i = 1:length(y_values) %calculating y matrix
+for i = 1:length(y_values)%calculating y matrix
     y_matrix(1) = y_matrix(1) + y_values(i)*sin(x_values(i));
     y_matrix(2) = y_matrix(2) + y_values(i)*cos(x_values(i));
 end
     
-for i = 1:length(x_values) %calculating x matrix
+for i = 1:length(x_values)%calculating x matrix
     x_matrix(1) = x_matrix(1) + sin(x_values(i))^2;
     x_matrix(2) = x_matrix(2) + sin(x_values(i))*cos(x_values(i));
     x_matrix(4) = x_matrix(4) + cos(x_values(i))^2;
 end
 x_matrix(3) = x_matrix(2); 
 
-coefficients_matrix = x_matrix\y_matrix; %calculating coefficients
+coefficients_matrix = x_matrix\y_matrix;%calculating coefficients
 result = -sin(coefficients_matrix(1)*sin(x_values + coefficients_matrix(2))) * 10^3.5;
-
-hold on; %graphics
+%result = -(sin(coef_matrix(1)*sin(x + coef_matrix(2)))) * 10^4;
+hold on;%graphics
 plot(x_actual, y_values, '*');
 plot(x_values, result, 'g', 'linewidth', 1);
 grid on; 
